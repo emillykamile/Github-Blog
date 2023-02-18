f67ec109945950aee2e7eca4a80cac61ba65182c
emillykamile <emillykamile.contato@gmail.com>
2023-02-18 15:44:49 -0300

    Criação do componente que irá compor o Blog

---
+++ src/components/Publicacoes/index.tsx
@@ -0,0 +1,30 @@
+// API
+
+
+// Componentes
+
+
+// Hooks
+
+
+
+export interface IPost {
+  titlePublicacao: string;
+  bodyPublicacao: string;
+  dateCreated: string;
+  numberID: number;
+  html_url: string;
+  comments: number;
+  user: {
+    login: string;
+  };
+}
+
+export function Blog() {
+
+    return (
+        <div>
+
+        </div>
+      );
+    }
