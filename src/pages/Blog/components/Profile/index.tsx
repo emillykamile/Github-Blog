22bca21650bd769edc32425c3a46b054180b4ff6
emillykamile <emillykamile.contato@gmail.com>
2023-02-27 13:17:45 -0300

    Adição do link externo GitHub ao Profile

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -1,9 +1,19 @@
-import { ProfileContainer, ProfilePicture } from "./styles";
+
+import { ExternalLink } from "../../../../components/ExternalLink";
+import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
 
 export function Profile(){
     return (
         <ProfileContainer>
             <ProfilePicture src="https://github.com/emillykamile.png"/>
+
+            <ProfileDetails>
+                <header>
+                    <h1>Emilly Kamile</h1>
+
+                    <ExternalLink text="Github" href="#"/>
+                </header>
+            </ProfileDetails>        
         </ProfileContainer>
     )
 }
