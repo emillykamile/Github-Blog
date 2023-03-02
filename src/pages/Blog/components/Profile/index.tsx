e57965e533af9e3b72d69841e960a25b9fb297a1
emillykamile <emillykamile.contato@gmail.com>
2023-03-02 13:31:24 -0300

    Finalizando Profile

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -1,4 +1,7 @@
 
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faBuilding } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ExternalLink } from "../../../../components/ExternalLink";
 import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
 
@@ -11,8 +14,23 @@ export function Profile(){
                 <header>
                     <h1>Emilly Kamile</h1>
 
-                    <ExternalLink text="Github" href="#"/>
+                    <ExternalLink text="Github" href="https://github.com/emillykamile" target="_blank"/>
                 </header>
+                <p>
+                Pertinaz, incansável e apaixonada naquilo que faço. Bilíngue em construção, empreendedora, autodidata,
+                amante da leitura, colecionadora de livros, e entusiasta do conhecimento, sou completamente apaixonada 
+                por tecnologia.
+                </p>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faGithub} />
+                        emillykamile
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faBuilding} />
+                        Freelancer
+                    </li>
+                </ul>
             </ProfileDetails>        
         </ProfileContainer>
     )
